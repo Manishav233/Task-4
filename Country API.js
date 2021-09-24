b0b02af11fec29166499a5a163b9fc33b6f61055
Manishav233 <66354254+Manishav233@users.noreply.github.com>
2021-09-24 12:01:53 +0530

    Updated the code, since previous rest countries link not working

--- Country API.js
+++ Country API.js
@@ -1,11 +1,11 @@
 var request= new XMLHttpRequest();
-request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.open('GET','https://restcountries.com/v3/all',true);
 request.send();
 request.onload=function(){
 var data=JSON.parse(this.response);
 console.log(data);
 var flag=data.forEach(element=> {
-console.log(element.flag);
+console.log(element.flags);
 });
 
-}
+};
