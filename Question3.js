f4e106164ac2d37e5a5d9b9bdc3ba38e3cc0cbb9
Manishav233 <66354254+Manishav233@users.noreply.github.com>
2021-09-26 01:12:40 +0530

    Previous Link was not working, Hence updated with new url.

--- Question3.js
+++ Question3.js
@@ -1,5 +1,5 @@
 var request= new XMLHttpRequest();
-request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json',true);
 request.send();
 request.onload=function(){
 var data=JSON.parse(this.response);
@@ -7,4 +7,4 @@ console.log(data);
 var flag=data.forEach(element=> {
 console.log(element.name+" , "+element.region+" , "+element.subregion+" , "+element.population);
 });
-}
+};
